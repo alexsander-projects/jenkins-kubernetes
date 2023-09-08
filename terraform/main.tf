31e575a58c65a974a7a8d3196ef3b3ef045231f5
nokorinotsubasa <alexsanderjuan13@gmail.com>
2023-09-08 10:24:58 -0300

    terraform added

---
+++ terraform/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.65.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.3.2"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "random" {
+}
+
+resource "azurerm_resource_group" "kube-rg" {
+  name     = "kube-rg"
+  location = "east us"
+}
+
+resource "azurerm_kubernetes_cluster" "kube-cluster" {
+  name                = "kube-cluster-aks1"
+  location            = azurerm_resource_group.kube-rg.location
+  resource_group_name = azurerm_resource_group.kube-rg.name
+  dns_prefix          = "kubeclusteraks1"
+
+  default_node_pool {
+    name       = "nodepool"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
